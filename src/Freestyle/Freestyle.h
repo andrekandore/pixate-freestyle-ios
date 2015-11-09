ce1055d98070f03527a35dc81959708fdaae44db
アンドレカンドレ <pyunpyun@mac.com>
2015-11-09 21:52:49 +0900

    First Working Build as a Modern Module

---
+++ src/Freestyle/Freestyle.h
@@ -0,0 +1,19 @@
+//
+//  Freestyle.h
+//  Freestyle
+//
+//  Created by アンドレ on 2015/11/09.
+//  Copyright © 2015年 Pixate, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Freestyle.
+FOUNDATION_EXPORT double FreestyleVersionNumber;
+
+//! Project version string for Freestyle.
+FOUNDATION_EXPORT const unsigned char FreestyleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Freestyle/PublicHeader.h>
+
+
